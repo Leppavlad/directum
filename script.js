274b2b12e3734a13f50f3d83a1197dca5b9a8a4d
Vlad <leppavlad@gmail.com>
2021-02-13 17:27:38 +0400

    Added select item

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ function formOutput(form) {
 }
 
 function formOfJson(file) {
-  document.querySelector('.form__container').innerHTML = "";
   let formObj = JSON.parse(file).form;
   let form = document.createElement("form");
       form.setAttribute("class", formObj.class);
   
-  if (formObj.name != "") {
+  if (formObj.name) {
     let formTitle = document.createElement('h2');
         formTitle.textContent = formObj.name;
     form.appendChild(formTitle);
   }
   document.querySelector('.form__container').appendChild(form);
   
-  if (formObj.postmessage != "") {
+  if (formObj.postmessage) {
     let formResponse = document.createElement('div');
         formResponse.setAttribute("class", "form-response");
         formResponse.innerHTML = formObj.postmessage;
@@ -29,46 +28,42 @@ function formOfJson(file) {
     formElements[i] = [];
 
     /* [0]Label - start */
-    if (item.attributes.label != "") {
+    if (item.attributes.label) {
       formElements[i].push(document.createElement('label'));
       formElements[i][0].textContent = item.attributes.label;
     } else {
       formElements[i][0] = "";
     }
-    /* [1]Label - end */
 
     /* [1]Input - start */
     if (item.type == "textarea") {
       formElements[i][1] = document.createElement('textarea');
-
-      formElements[i][1].setAttribute("name", item.attributes.name);
-      formElements[i][1].setAttribute("placeholder", item.attributes.placeholder);
-      formElements[i][1].setAttribute("class", item.attributes.class);
-      formElements[i][1].setAttribute("value", item.attributes.value);
-      
-      item.attributes.required == true ?
-        formElements[i][1].setAttribute("required", true) : null;
-      item.attributes.disabled == true ?
-        formElements[i][1].setAttribute("disabled", true) : null;
+    } else if (item.type == "select") {
+      formElements[i][1] = document.createElement('select');
+      item.attributes.options.forEach(option => {
+        let optionElement = document.createElement("option")
+            optionElement.setAttribute("value", option.value);
+            optionElement.textContent = option.text;
+        (option.selected) ?
+          optionElement.setAttribute("selected", "") : null;
+        formElements[i][1].appendChild(optionElement);
+      })
     } else {
       formElements[i][1] = document.createElement('input');
-      if (item.type == "submit") {
-        formElements[i][1].setAttribute("type", "submit");
-        formElements[i][1].setAttribute("class", item.attributes.class);
-        formElements[i][1].setAttribute("value", item.attributes.value);
-      } else {
-        formElements[i][1].setAttribute("type", item.type);
-        formElements[i][1].setAttribute("name", item.attributes.name);
-        formElements[i][1].setAttribute("placeholder", item.attributes.placeholder);
-        formElements[i][1].setAttribute("class", item.attributes.class);
-        formElements[i][1].setAttribute("value", item.attributes.value);
-        
-      item.attributes.required == true ?
-        formElements[i][1].setAttribute("required", true) : null;
-      item.attributes.disabled == true ?
-        formElements[i][1].setAttribute("disabled", true) : null;
-      }
+      formElements[i][1].setAttribute("type", item.type);
     }
+    formElements[i][1].setAttribute("class", item.attributes.class);
+    formElements[i][1].setAttribute("name", item.attributes.name);
+
+    (item.type != "submit") ? (item.type != "checkbox") ?
+      formElements[i][1].setAttribute("placeholder", item.attributes.placeholder) : null :
+      formElements[i][1].setAttribute("value", item.attributes.value);
+    (item.type == "checkbox" && item.attributes.checked) ?
+      formElements[i][1].setAttribute("checked", "") : null;
+    (item.attributes.required) ?
+      formElements[i][1].setAttribute("required", true) : null;
+    (item.attributes.disabled) ?
+      formElements[i][1].setAttribute("disabled", true) : null;        
     /* [1]Input - end */
 
     if (formElements[i][0] == "") {
